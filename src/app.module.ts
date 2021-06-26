1e2f556c379264726c8f78620efbae5347ec1794
kwaiga <i.kwapisz@codeandpepper.com>
2021-06-27 01:06:27 +0200

    feat(swb): initialize episodes resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CharactersModule } from './characters/characters.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { EpisodesModule } from './episodes/episodes.module';
 import ormconfig from './ormconfig';
 
 @Module({
-  imports: [CharactersModule, TypeOrmModule.forRoot(ormconfig)],
+  imports: [CharactersModule, TypeOrmModule.forRoot(ormconfig), EpisodesModule],
   controllers: [AppController],
   providers: [AppService],
 })
